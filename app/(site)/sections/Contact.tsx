370436dd25379934b9756d2acf453f4f07f82840
Harish Subramanian <47149879+HarishS99@users.noreply.github.com>
2025-10-25 23:49:23 +0530

    Update Contact.tsx

--- app/(site)/sections/Contact.tsx
+++ app/(site)/sections/Contact.tsx
@@ -6,6 +6,7 @@ export default function Contact() {
         Let’s connect — open to collaborations and impactful tech roles.
       </p>
       <div className="space-y-1 opacity-80">
+        <p>Phone: +91-9481579529</p>
         <p>Email: <a className="underline" href="mailto:99harishs99@gmail.com">99harishs99@gmail.com</a></p>
         <p><a className="underline" href="https://www.linkedin.com/in/harish-subramanian-a246a016b/" target="_blank">Connect on LinkedIn</a></p>
       </div>

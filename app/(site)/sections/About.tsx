b701b522495ac9360ffc879142d60df6c9e3c5d7
Harish Subramanian <47149879+HarishS99@users.noreply.github.com>
2025-10-26 16:24:04 +0530

    Update About.tsx

--- app/(site)/sections/About.tsx
+++ app/(site)/sections/About.tsx
@@ -3,8 +3,8 @@ export default function About() {
     <section id="about" className="max-w-3xl mx-auto mt-16 mb-16">
       <h2 className="text-2xl font-semibold mb-2">About Me</h2>
       <p className="opacity-80 mb-4">
-        Tech Evangelist with 4+ years in Infrastructure, Data Protection and Enterprise Backup. Currently at Commvault delivering enterprise demos,
-        POCs and GTM strategy. Pursuing Advanced Business Analytics at IIM Ahmedabad to sharpen strategic and analytical decision-making.
+        Tech Evangelist with 5+ years experience in IT Infrastructure, Data Protection, Cyber Security and Enterprise Backup. Currently at Commvault designing top notch enterprise grade solutions while also delivering demos,
+        and Proof of Values to the business stakeholders. Pursuing my MBA in Business Analytics and AI from IIM Ahmedabad to sharpen my strategic and analytical decision-making.
       </p>
     </section>
   );

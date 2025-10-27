5df01def662706acc3ecd5de3874be91e2f2b00f
Harish Subramanian <47149879+HarishS99@users.noreply.github.com>
2025-10-27 12:04:41 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,36 @@
 import type { Config } from "tailwindcss";
+
 const config: Config = {
   darkMode: "class",
   content: ["./app/**/*.{ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["ui-sans-serif", "system-ui", "Segoe UI", "Roboto", "Inter", "Arial", "Noto Sans", "Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"]
-      }
+        sans: [
+          "ui-sans-serif",
+          "system-ui",
+          "Segoe UI",
+          "Roboto",
+          "Inter",
+          "Arial",
+          "Noto Sans",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+        ],
+      },
+      animation: {
+        fadeIn: "fadeIn 0.8s ease-out forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
